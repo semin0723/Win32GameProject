d8adc3858f3ba622f50d2c9169ecfa2ba8aab94c
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-04-11 16:14:44 +0900

    Vector3 기본 생성자 추가.

--- Win32GameProject/Vector3.cpp
+++ Win32GameProject/Vector3.cpp
@@ -1,5 +1,7 @@
 #include "Vector3.h"
 
+Vector3::Vector3() {}
+
 explicit Vector3::Vector3(float x, float y, float z) {
 	_x = x;
 	_y = y;
--- Win32GameProject/Vector3.h
+++ Win32GameProject/Vector3.h
@@ -5,18 +5,18 @@ public:
 	float _x;
 	float _y;
 	float _z;
+	Vector3();
+	explicit Vector3(float x, float y, float z);
 
-	explicit Vector3(float x, float y, float z) {}
+	void operator=	(const Vector3& v);
 
-	void operator=	(const Vector3& v) {}
+	Vector3 operator+=	(const Vector3& v);
+	Vector3 operator-=	(const Vector3& v);
+	Vector3 operator*=	(const float& scalar);
+	Vector3 operator/=	(const float& scalar);
 
-	Vector3 operator+=	(const Vector3& v) {}
-	Vector3 operator-=	(const Vector3& v) {}
-	Vector3 operator*=	(const float& scalar) {}
-	Vector3 operator/=	(const float& scalar) {}
-
-	Vector3 operator+	(const Vector3& v) const {}
-	Vector3 operator-	(const Vector3& v) const {}
-	Vector3 operator*	(const float& scalar) const {}
-	Vector3 operator/	(const float& scalar) const {}
+	Vector3 operator+	(const Vector3& v) const;
+	Vector3 operator-	(const Vector3& v) const;
+	Vector3 operator*	(const float& scalar) const;
+	Vector3 operator/	(const float& scalar) const;
 };

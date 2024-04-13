f21d9855bb649ad868f75d53a4048f521df4f6b6
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-04-13 19:19:08 +0900

    모든 오브젝트를 총괄하는 클래스 생성.

--- Win32GameProject/GameObject.h
+++ Win32GameProject/GameObject.h
@@ -1,12 +1,26 @@
 #pragma once
 #include "Vector3.h"
+#include "Win32GameProject.h"
 
 class GameObject
 {
 public:
-	Vector3 _location;
-	int _ResourceID;
 	GameObject();
+	virtual ~GameObject();
+	void SetLocation(const Vector3& loc) { _location = loc; }
+	void SetResourceID(const int id) { _ResourceID = id; }
+	void SetScale(const Vector3& scale) { _scale = scale; }
+	Vector3 GetLocation() const { return Vector3(_location); }
+	Vector3 GetScale() const { return Vector3(_scale); }
+	int GetResourceID() const { return _ResourceID; }
+
+	virtual void update() = 0;
+	virtual void render(HDC hdc);
+
+private:
+	Vector3 _location;
+	Vector3 _scale;
+	int _ResourceID;
 };
 
 /*

65e2be9b052668cfefb5eebe80f11f6dd4c9e294
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-04-13 20:50:25 +0900

    Vector의 구조 일부 잘못된 점 수정

--- Win32GameProject/Vector.hpp
+++ Win32GameProject/Vector.hpp
@@ -6,40 +6,43 @@ public:
 	Vector() {
 		_size = 0;
 		_flg = 0;
-		data = new T(MAX_SIZE);
+		_data = new T[MAX_SIZE];
 	}
 	Vector(int size) {
 		_size = 0;
 		_flg = 0;
-		data = new T(size);
+		_data = new T[size];
 	}
-	Vector(int size, T filldata) {
+	Vector(int size, const T& filldata) {
 		_size = size;
 		_flg = _size - 1;
-		data = new T(size);
+		_data = new T[size];
 		for (int i = 0; i < _size; i++) {
-			data[i] = filldata;
+			_data[i] = filldata;
 		}
 	}
-	void push_back(T value) {
+	void push_back(const T& value) {
 		if (_flg == _size) {
 			resize();
 		}
 		data[_flg] = value;
 		_flg++;
 	}
-	T operator[](const int& idx) const {
-		return data[idx];
+	const T& operator[](int idx) const {
+		return _data[idx];
+	}
+	T& operator[](int idx) {
+		return _data[idx];
 	}
 
 private:
 	int MAX_SIZE = 10;
 	int _size;
 	int _flg;
-	T* data;
+	T* _data;
 
 	void resize() {
-		T* tmp = new T(MAX_SIZE * 2);
+		T* tmp = new T[MAX_SIZE * 2];
 		for (int i = 0; i < _size; i++) {
 			tmp[i] = data[i];
 		}

43aaac4b4659822352598ca0e9da7d9a2f9eb2e1
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-04-17 14:45:24 +0900

    queue container를 추가합니다.
    STL을 사용하지 않고 만들어야 하기에 필요한 컨테이너의 개념을 제작하여 추가합니다.

---
+++ Win32GameProject/queue.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+template <typename T>
+class queue {
+public:
+    queue() {
+        _start = _end = _size = 0;
+        MAX_SIZE = 10;
+        _data = new T[MAX_SIZE];
+    }
+    ~queue() {
+        if (_data) delete[] _data;
+    }
+    bool empty() {
+        return _start == _end;
+    }
+    int size() {
+        return _size;
+    }
+    void push(T e) {
+        _data[_end] = e;
+        _size++;
+        _end = (_end + 1) % MAX_SIZE;
+        if (end == start) {
+            resize();
+        }
+    }
+    int front() {
+        return _data[_start];
+    }
+    void pop() {
+        _start = (_start + 1) % MAX_SIZE;
+        _size--;
+    }
+private:
+	int _size;
+    int _start;
+    int _end;
+    int MAX_SIZE;
+	T* _data;
+
+    void resize() {
+        T* tmp = new T[MAX_SIZE * 2];
+        int flg = 0;
+        for (int i = 0; i < MAX_SIZE; i++) {
+            tmp[flg] = _data[(_start + i) % MAX_SIZE];
+            flg++;
+        }
+        delete[] data;
+        _data = tmp;
+        _start = 0; _end = flg;
+        MAX_SIZE *= 2;
+    }
+};

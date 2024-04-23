b9cd13ff1b69fe7015906f354e7e2aae2d033f6e
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-04-23 16:49:30 +0900

    미완성 코드 주석 정리

--- Win32GameProject/ResourceManager.cpp
+++ Win32GameProject/ResourceManager.cpp
@@ -65,67 +65,67 @@ const char* ResourceManager::GetResourceName(int state) {
 	}
 }
 
-const wchar_t* ResourceManager::GetResourceFilePath(int state)
-{
-	const wchar_t* ResourcePath;
-	switch (state) {
-	case 0:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"UnOpenedTile_Focus.bmp");
-		break;
-	case 1:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_One.bmp");
-		break;
-	case 2:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Two.bmp");
-		break;
-	case 3:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Three.bmp");
-		break;
-	case 4:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Four.bmp");
-		break;
-	case 5:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Five.bmp");
-		break;
-	case 6:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Six.bmp");
-		break;
-	case 7:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Seven.bmp");
-		break;
-	case 8:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Eight.bmp");
-		break;
-	case 10:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Mine.bmp");
-		break;
-	case 11:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"UnOpenedTile.bmp");
-		break;
-	case 12:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Flag.bmp");
-		break;
-	case 13:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Died_Face.bmp");
-		break;
-	case 14:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Win_Face.bmp");
-		break;
-	case 15:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"UnOpenedTile.bmp");
-		break;
-	case 16:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Play_Button.bmp");
-		break;
-	case 17:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Play_Button_Clicked.bmp");
-		break;
-	case 18:
-		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Smile_Face.bmp");
-		break;
-	default:
-		ResourcePath = L"";
-	}
-
-	return ResourcePath;
-}
+//const wchar_t* ResourceManager::GetResourceFilePath(int state)
+//{
+//	const wchar_t* ResourcePath;
+//	switch (state) {
+//	case 0:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"UnOpenedTile_Focus.bmp");
+//		break;
+//	case 1:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_One.bmp");
+//		break;
+//	case 2:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Two.bmp");
+//		break;
+//	case 3:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Three.bmp");
+//		break;
+//	case 4:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Four.bmp");
+//		break;
+//	case 5:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Five.bmp");
+//		break;
+//	case 6:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Six.bmp");
+//		break;
+//	case 7:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Seven.bmp");
+//		break;
+//	case 8:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Opened_Eight.bmp");
+//		break;
+//	case 10:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Mine.bmp");
+//		break;
+//	case 11:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"UnOpenedTile.bmp");
+//		break;
+//	case 12:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Flag.bmp");
+//		break;
+//	case 13:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Died_Face.bmp");
+//		break;
+//	case 14:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Win_Face.bmp");
+//		break;
+//	case 15:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"UnOpenedTile.bmp");
+//		break;
+//	case 16:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Play_Button.bmp");
+//		break;
+//	case 17:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Play_Button_Clicked.bmp");
+//		break;
+//	case 18:
+//		ResourcePath = PathManager::GetInstance()->GetImagePath(L"Smile_Face.bmp");
+//		break;
+//	default:
+//		ResourcePath = L"";
+//	}
+//
+//	return ResourcePath;
+//}
